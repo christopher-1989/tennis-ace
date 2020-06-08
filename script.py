48b2d380fcb6ae3d9d68882871bf103ed51913c7
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-06-08 19:06:31 +1000

    final creation of regression models

--- script.py
+++ script.py
@@ -19,14 +19,7 @@ print(columns)
 
 # perform single feature linear regressions here:
 
-x = df[['Year', 'FirstServe', 'FirstServePointsWon',
-       'FirstServeReturnPointsWon', 'SecondServePointsWon',
-       'SecondServeReturnPointsWon', 'Aces', 'BreakPointsConverted',
-       'BreakPointsFaced', 'BreakPointsOpportunities', 'BreakPointsSaved',
-       'DoubleFaults', 'ReturnGamesPlayed', 'ReturnGamesWon',
-       'ReturnPointsWon', 'ServiceGamesPlayed', 'ServiceGamesWon',
-       'TotalPointsWon', 'TotalServicePointsWon', 'Losses', 'Winnings',
-       'Ranking']]
+x = df[['Aces']]
 y = df[['Wins']]
 
 
@@ -37,10 +30,10 @@ model = lm.fit(x_train, y_train)
 
 y_predict= lm.predict(x_test)
 
-print("Train score:")
+print("One feature linear regression Train score:")
 print(lm.score(x_train, y_train))
 
-print("Test score:")
+print("One feature linear regression Test score:")
 print(lm.score(x_test, y_test))
 plt.scatter(y_test, y_predict)
 plt.xlabel("Actual win:")
@@ -50,42 +43,59 @@ print(model.coef_)
 plt.show()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ## perform two feature linear regressions here:
 
 
+x = df[['Aces', 'DoubleFaults']]
+y = df[['Wins']]
 
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+lm = LinearRegression()
 
+model = lm.fit(x_train, y_train)
 
+y_predict= lm.predict(x_test)
 
+print("Two feature linear regression Train score:")
+print(lm.score(x_train, y_train))
 
-
-
-
-
-
-
-
-
-
-
-
-
+print("Two feature linear regression Test score:")
+print(lm.score(x_test, y_test))
+plt.scatter(y_test, y_predict)
+plt.xlabel("Actual win:")
+plt.ylabel("Predicted win:")
+plt.title("Actual win vs Predicted win")
+print(model.coef_)
+plt.show()
 
 
 ## perform multiple feature linear regressions here:
+x = df[['Year', 'FirstServe', 'FirstServePointsWon',
+       'FirstServeReturnPointsWon', 'SecondServePointsWon',
+       'SecondServeReturnPointsWon', 'Aces', 'BreakPointsConverted',
+       'BreakPointsFaced', 'BreakPointsOpportunities', 'BreakPointsSaved',
+       'DoubleFaults', 'ReturnGamesPlayed', 'ReturnGamesWon',
+       'ReturnPointsWon', 'ServiceGamesPlayed', 'ServiceGamesWon',
+       'TotalPointsWon', 'TotalServicePointsWon', 'Losses', 'Winnings',
+       'Ranking']]
+y = df[['Wins']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+lm = LinearRegression()
+
+model = lm.fit(x_train, y_train)
+
+y_predict= lm.predict(x_test)
+
+print("Multiple linear regression train score:")
+print(lm.score(x_train, y_train))
+
+print("Multiple linear regression test score:")
+print(lm.score(x_test, y_test))
+plt.scatter(y_test, y_predict)
+plt.xlabel("Actual win:")
+plt.ylabel("Predicted win:")
+plt.title("Actual win vs Predicted win")
+print(model.coef_)
+plt.show()
