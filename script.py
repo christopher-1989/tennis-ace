40f36477c77aa7b20e011f1936819b32b001844a
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-06-08 19:00:03 +1000

    Multiple linear regression

--- script.py
+++ script.py
@@ -6,43 +6,48 @@ from sklearn.linear_model import LinearRegression
 
 # load and investigate the data here:
 df = pd.read_csv("tennis_stats.csv")
-print(df.head())
+#print(df.head())
 columns = df.columns
 
-
 # perform exploratory analysis here:
+print(columns)
+#print(df[['Wins']].describe())
+#print(df[['Losses']].describe())
+
+#print(df[['Aces']].describe())
 
 
+# perform single feature linear regressions here:
 
+x = df[['Year', 'FirstServe', 'FirstServePointsWon',
+       'FirstServeReturnPointsWon', 'SecondServePointsWon',
+       'SecondServeReturnPointsWon', 'Aces', 'BreakPointsConverted',
+       'BreakPointsFaced', 'BreakPointsOpportunities', 'BreakPointsSaved',
+       'DoubleFaults', 'ReturnGamesPlayed', 'ReturnGamesWon',
+       'ReturnPointsWon', 'ServiceGamesPlayed', 'ServiceGamesWon',
+       'TotalPointsWon', 'TotalServicePointsWon', 'Losses', 'Winnings',
+       'Ranking']]
+y = df[['Wins']]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## perform single feature linear regressions here:
-
-x = df[['Aces']]
-y = df[['wins']]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
 lm = LinearRegression()
 
+model = lm.fit(x_train, y_train)
 
+y_predict= lm.predict(x_test)
 
+print("Train score:")
+print(lm.score(x_train, y_train))
+
+print("Test score:")
+print(lm.score(x_test, y_test))
+plt.scatter(y_test, y_predict)
+plt.xlabel("Actual win:")
+plt.ylabel("Predicted win:")
+plt.title("Actual win vs Predicted win")
+print(model.coef_)
+plt.show()
 
 
 
